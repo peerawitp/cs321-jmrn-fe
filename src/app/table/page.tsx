3f13fda05311797d02d2fec97acaa9c4794ada39
Nemo Nakorn <75536335+Nemo-netidol@users.noreply.github.com>
2024-10-02 17:58:52 +0700

    Fixed product-management page (add button)

--- src/app/table/page.tsx
+++ src/app/table/page.tsx
@@ -1,27 +1,70 @@
-'use client'
-import React, { useContext, useState } from 'react';
-import TableRow from '../components/TableRow';
-import { product } from '../../../public/data/data';
+"use client";
+import React, { useContext, useState } from "react";
+import TableRow from "../components/TableRow";
+import { product } from "../../../public/data/data";
 import { Context } from "../TableContext";
+import UpdateForm from "./UpdateForm";
+import CreateForm from "./CreateForm";
 
 const Table = () => {
-    
-    const { setID } = useContext(Context);
+    const [showCreatePopUp, setShowCreatePopUp] = useState(false);
+    const [showUpdatePopUp, setShowPopUp] = useState(false);
+    const [id, setID] = useState<string | null>(null);
 
     const [selectedID, setSelectedID] = useState<string | null>(null); // Track selected row
 
     const handleRowClick = (id: string) => {
         setSelectedID(id); // Update selected row ID
         setID(id);
-        console.log("change id from Table.tsx: " + id)
+        console.log("change id from Table.tsx: " + id);
     };
 
 
+    const handleUpdateOpenPopUp = () => {
+        if (id === null) {
+            alert("Select product first");
+        }
+        else {
+            setShowPopUp(true); // Show pop-up
+        }
+
+    };
+    const handleUpdateClosePopUp = () => {
+        setShowPopUp(false); // Show pop-up
+    };
+
+    const handleCreateOpenPopUp = () => {
+        setShowCreatePopUp(true)
+    }
+
+    const handleCreateClosePopUp = () => {
+        setShowCreatePopUp(false)
+    }
+
 
     return (
-        <div className="flex justify-center text-center">
+        <div className="flex">
             <div className="overflow-x-auto w-full bg-white text-black">
-                <table className="min-w-full table-auto">
+                {/* button */}
+                <div className="mt-[50px] mb-[10px] flex justify-start">
+                    <button
+                        className="mx-3 btn bg-[#387ADF] hover:bg-[#2558a5] text-white"
+                        onClick={handleCreateOpenPopUp}
+                    >
+                        Create
+                    </button>
+                    <button
+                        className="mr-3 btn btn-ghost bg-white"
+                        onClick={handleUpdateOpenPopUp}
+                    >
+                        Update
+                    </button>
+                    <button className="mr-3 btn bg-[#EC5A5A] hover:bg-[#ff4a4a] text-white">
+                        Delete
+                    </button>
+                </div>
+
+                <table className="min-w-full table-auto justify-center text-center">
                     <thead className="text-xs sm:text-sm bg-gray-100 border-b border-gray-300">
                         <tr>
                             <th className="p-2 border-r border-gray-300">ID</th>
@@ -43,16 +86,31 @@ const Table = () => {
                         </tr>
                     </thead>
                     <tbody>
-                    {product.map((item) => (
-                            <TableRow 
-                                key={item.id} 
-                                product={item} 
+                        {product.map((item) => (
+                            <TableRow
+                                key={item.id}
+                                product={item}
                                 isSelected={selectedID === item.id} // Pass selection state
                                 onRowClick={handleRowClick} // Pass row click handler
                             />
                         ))}
                     </tbody>
                 </table>
+                {showCreatePopUp && (
+                    <div className="fixed inset-0 flex justify-center items-start bg-black bg-opacity-50 z-50">
+                        <div className="mt-20">
+                            <CreateForm closePopUp={handleCreateClosePopUp} />
+                        </div>
+                    </div>
+                )}
+
+                {showUpdatePopUp && (
+                    <div className="fixed inset-0 flex justify-center items-start bg-black bg-opacity-50 z-50">
+                        <div className="mt-20">
+                            <UpdateForm closePopUp={handleUpdateClosePopUp} />
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
