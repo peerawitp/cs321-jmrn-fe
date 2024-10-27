44df5e371dca0836e544b36fd2b177c919150e71
Peerawit Pharkdeepinyo <peerawitp@protonmail.com>
2024-10-27 16:38:43 +0700

    feat(order-status): remove PROCESSING status

--- src/lib/orderStatusText.ts
+++ src/lib/orderStatusText.ts
@@ -2,8 +2,6 @@ import { OrderStatus } from "@/interfaces/Order";
 
 export const getOrderStatusText = (status: OrderStatus) => {
   switch (status) {
-    case OrderStatus.PROCESSING:
-      return "Processing";
     case OrderStatus.WAITING_PAYMENT:
       return "Waiting for Payment";
     case OrderStatus.WAITING_PAYMENT_CONFIRMATION:
